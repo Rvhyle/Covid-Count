437b37e586a0c58cad2dd133d0682aaa90992257
Ryle Urcia <ryleurcia@ryles-mbp.home>
2020-11-18 22:18:37 -0500

    Added Region component to home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,24 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { fetchCases } from '../Util';
 // Components
 import Cases from '../Components/Cases';
+import Regions from '../Components/Regions';
 // Styled Components
 import { Home } from '../Styled-Components/StyledHome';
 
 const HomePage = () => {
     const [usCases, setUsCases] = useState(0);
+    const [usDeaths,setUsDeaths] = useState(0);
 
     // Call To API
     useEffect(() => {
         fetchCases()
             .then(data => {
                 setUsCases(data.positive)
+                setUsDeaths(data.death)
             })
             .catch(err => console.log(err));
+
+        console.log("Mounted");
+        
     });
 
     return (
         <Home>
-            <Cases usCases={usCases} setUsCases={setUsCases} />
+            <Cases 
+                usCases={usCases} 
+                setUsCases={setUsCases} 
+                usDeaths = {usDeaths} 
+                setUsDeaths = {setUsDeaths}
+            />
+            <Regions/>
         </Home>
     )
 }
