b85089bac55d0b46b27efb34ff15a62a3c4a8ec6
Ryle <itsryle.urcia@gmail.com>
2020-11-19 22:37:29 -0500

    Small fixes

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -8,9 +8,9 @@ import { Home } from '../Styled-Components/StyledHome';
 
 const HomePage = () => {
     const [usCases, setUsCases] = useState(0);
-    const [usDeaths,setUsDeaths] = useState(0);
+    const [usDeaths, setUsDeaths] = useState(0);
 
-    // Call To API
+    // Will fetch API whenever component gets rendered/updated
     useEffect(() => {
         fetchCases()
             .then(data => {
@@ -20,18 +20,18 @@ const HomePage = () => {
             .catch(err => console.log(err));
 
         console.log("Mounted");
-        
+
     });
 
     return (
         <Home>
-            <Cases 
-                usCases={usCases} 
-                setUsCases={setUsCases} 
-                usDeaths = {usDeaths} 
-                setUsDeaths = {setUsDeaths}
+            <Cases
+                usCases={usCases}
+                setUsCases={setUsCases}
+                usDeaths={usDeaths}
+                setUsDeaths={setUsDeaths}
             />
-            <Regions/>
+            <Regions />
         </Home>
     )
 }
