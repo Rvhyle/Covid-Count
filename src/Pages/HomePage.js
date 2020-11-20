c01f1337f5c974661daecca26f93b7a74a63d704
Ryle <itsryle.urcia@gmail.com>
2020-11-20 13:41:19 -0500

    got rid of fetch function and imported Axios to fetch the data, created new states and passed it to respective components

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { fetchCases } from '../Util';
+import axios from 'axios';
 // Components
 import Cases from '../Components/Cases';
 import Regions from '../Components/Regions';
@@ -9,27 +9,59 @@ import { Home } from '../Styled-Components/StyledHome';
 const HomePage = () => {
     const [usCases, setUsCases] = useState(0);
     const [usDeaths, setUsDeaths] = useState(0);
+    const [usNegatives, setUsNegatives] = useState(0);
+    const [usRecovered, setUsRecovered] = useState(0);
+    const [usHospitalized, setUsHospitalized] = useState(0);
+    const [usDeathIncrease, setUsDeathIncrease] = useState(0);
+    const [usCaseIncrease, setUsCaseIncrease] = useState(0);
+    const [usTestTotal, setUsTestTotal] = useState(0);
+    // Fetch API from web
+    const getData = async () => {
+        try {
+            let res = await axios.get("//api.covidtracking.com/v1/us/current.json")
+            // Capturing data from request
+            let cases = res.data[0].positive;
+            let death = res.data[0].death;
+            let negative = res.data[0].negative;
+            let recovered = res.data[0].recovered;
+            let hospitalized = res.data[0].hospitalized;
+            let deathIncrease = res.data[0].deathIncrease;
+            let caseIncrease = res.data[0].positiveIncrease;
+            let testTotal = res.data[0].totalTestResults;
 
-    // Will fetch API whenever component gets rendered/updated
+            console.log(res.data[0]);
+
+            // Setting States
+            setUsCases(cases);
+            setUsDeaths(death);
+            setUsNegatives(negative);
+            setUsRecovered(recovered);
+            setUsHospitalized(hospitalized);
+            setUsDeathIncrease(deathIncrease);
+            setUsCaseIncrease(caseIncrease);
+            setUsTestTotal(testTotal);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    // Whenever Component Mounts/Updates
     useEffect(() => {
-        fetchCases()
-            .then(data => {
-                setUsCases(data.positive)
-                setUsDeaths(data.death)
-            })
-            .catch(err => console.log(err));
-
-        console.log("Mounted");
-
-    });
+        getData();
+    }, []);
 
     return (
         <Home>
+            <h1 className="title">USA TOTALS</h1>
             <Cases
                 usCases={usCases}
-                setUsCases={setUsCases}
                 usDeaths={usDeaths}
-                setUsDeaths={setUsDeaths}
+                usNegatives={usNegatives}
+                usRecovered={usRecovered}
+                usHospitalized={usHospitalized}
+                usDeathIncrease={usDeathIncrease}
+                usCaseIncrease={usCaseIncrease}
+                usTestTotal={usTestTotal}
             />
             <Regions />
         </Home>
