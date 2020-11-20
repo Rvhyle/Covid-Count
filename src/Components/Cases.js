99db20416640418a7b6a0c0a60df6fdcaeefa40d
Ryle Urcia <ryleurcia@ryles-mbp.home>
2020-11-19 21:48:14 -0500

    Formatted the data for betterreadability

--- src/Components/Cases.js
+++ src/Components/Cases.js
@@ -8,11 +8,12 @@ const Cases = ({ usCases, setUsCases, usDeaths, setUsDeaths}) => {
         <CasesStyled>
             <div className="data cases">
                 <h1>USA Total Cases:</h1>
-                <h2>{usCases}</h2>
+                {/* Formatted data to be seperated with commas for readability */}
+                <h2>{usCases.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</h2>
             </div>
             <div className="data deaths">
                 <h1>USA Total Deaths:</h1>
-                <h2>{usDeaths}</h2>
+                <h2>{usDeaths.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</h2>
             </div>
         </CasesStyled>
     )
